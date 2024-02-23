a3ed1f1b4f195e059bd3df9876839a7ff54be064
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-02-23 16:16:02 +0900

    トップページとshopsのscaffold作成

---
+++ db/migrate/20240223070252_create_shops.rb
@@ -0,0 +1,15 @@
+class CreateShops < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shops do |t|
+      t.string :name
+      t.string :address
+      t.string :string
+      t.string :phone_number
+      t.string :url
+      t.string :opening_hours
+      t.integer :chain
+
+      t.timestamps
+    end
+  end
+end
